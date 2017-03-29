60aeb13d034acde1874d369dfbefff5a72f7ff90
sikora195703 <195703@edu.p.lodz.pl>
2017-03-29 08:37:27 +0200

    fix

---
+++ client/src/main/java/zaawjava/controllers/RegistrationController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zaawjava.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Wiktor
+ */
+public class RegistrationController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    public void setParameters(Channel channel, EventLoopGroup group) {
+    }
+}

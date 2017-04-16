6db609f70092d073bd335c9698eb95c4982ba754
AdrianNad <adriannad@interia.pl>
2017-04-17 01:08:57 +0200

    nothing important

--- server/src/main/java/zaawjava/Main.java
+++ server/src/main/java/zaawjava/Main.java
@@ -6,7 +6,7 @@ import org.hibernate.cfg.Configuration;
 import model.User;
 import zaawjava.model.DatabaseConnector;
 
-public class Main {
+public class Main{
 	public static SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(User.class).buildSessionFactory();
 
     public static void main(String[] args){

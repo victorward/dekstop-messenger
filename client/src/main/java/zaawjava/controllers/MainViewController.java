628444e09888a3c5ad0c7723c944513ee2363e1d
Yuriy Babyak <199184@edu.p.lodz.pl>
2017-04-25 20:43:07 +0200

    Teraz przy wylogowaniu usuwa sie uzytkownik z listy zalogowanych

--- client/src/main/java/zaawjava/controllers/MainViewController.java
+++ client/src/main/java/zaawjava/controllers/MainViewController.java
@@ -7,13 +7,16 @@ package zaawjava.controllers;
 
 import io.netty.channel.Channel;
 import io.netty.channel.EventLoopGroup;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.stage.Stage;
+import model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import zaawjava.ScreensManager;
+import zaawjava.services.SocketService;
 import zaawjava.services.UserService;
 
 import java.io.IOException;
@@ -28,6 +31,12 @@ public class MainViewController implements Initializable {
     private EventLoopGroup group;
     private ScreensManager screensManager;
     private UserService userService;
+    private final SocketService socketService;
+
+    @Autowired
+    public MainViewController(SocketService socketService) {
+        this.socketService = socketService;
+    }
 
     @Autowired
     public void setUserService(UserService userService) {
@@ -55,8 +64,21 @@ public class MainViewController implements Initializable {
 
     @FXML
     void onLogoutClick(ActionEvent event) throws IOException {
+        logOutUser(userService.getUser());
         screensManager.goToLoginView();
     }
 
-
+    private void logOutUser(User user) {
+        socketService.emit("loggedOutUser", user).whenComplete((msg, ex) -> {
+            if (ex == null) {
+                if ("loggedOutUser".equals(msg)) {
+                    System.out.println("Logged out");
+                } else {
+                    Platform.runLater(() -> System.out.println("Logout failed"));
+                }
+            } else {
+                Platform.runLater(() -> System.out.println("Failed during logout actual user"));
+            }
+        });
+    }
 }

b379147ed87108221f461e21b7680d9665efa313
sikora195703 <195703@edu.p.lodz.pl>
2017-04-29 13:05:03 +0200

    dodanie pominiętych klas

---
+++ server/src/main/java/zaawjava/model/Language.java
@@ -0,0 +1,47 @@
+package zaawjava.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "language")
+public class Language implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "language_name")
+    private String languageName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLanguageName() {
+        return languageName;
+    }
+
+    public void setLanguageName(String languageName) {
+        this.languageName = languageName;
+    }
+
+    public Language(String languageName) {
+        super();
+        this.languageName = languageName;
+    }
+
+    public Language() {
+        super();
+    }
+
+    @Override
+    public String toString() {
+        return "Language [id=" + id + ", languageName=" + languageName + "]";
+    }
+
+}

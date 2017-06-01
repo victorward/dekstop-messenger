e69271e1fe1a5ae229ac08fcdfb8cd999beb5a2d
ciak195568 <195568@edu.p.lodz.pl>
2017-06-01 18:01:03 +0200

    przygotowanie struktury do testów UserService

---
+++ server/src/test/zaawjava/server/services/UserServiceTest.java
@@ -0,0 +1,51 @@
+package zaawjava.server.services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class UserServiceTest {
+    @Test
+    public void addUserToLoggedList1() throws Exception {
+        UserService userService = new UserService();
+
+    }
+
+    @Test
+    public void deleteUserFromLoggedList() throws Exception {
+    }
+
+    @Test
+    public void deleteUserFromLoggedList1() throws Exception {
+    }
+
+    @Test
+    public void getNumberOfLoggedUsers() throws Exception {
+    }
+
+    @Test
+    public void checkIfLogged() throws Exception {
+    }
+
+    @Test
+    public void printUserList() throws Exception {
+    }
+
+    @Test
+    public void getListOfLoggedUsers() throws Exception {
+    }
+
+    @Test
+    public void getUserChannel() throws Exception {
+    }
+
+    @Test
+    public void getUserByChannel() throws Exception {
+    }
+
+    @Test
+    public void addUserToLoggedList() throws Exception {
+    }
+
+}

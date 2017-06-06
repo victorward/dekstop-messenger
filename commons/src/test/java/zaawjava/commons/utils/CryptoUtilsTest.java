70e29eff00854f81c999b51a11675e28f498f972
ciak195568 <195568@edu.p.lodz.pl>
2017-06-06 13:40:19 +0200

    CryptoUtilsTest

---
+++ commons/src/test/java/zaawjava/commons/utils/CryptoUtilsTest.java
@@ -0,0 +1,18 @@
+package zaawjava.commons.utils;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class CryptoUtilsTest {
+    @Test
+    public void encryptPassword() throws Exception {
+        String encrypted = CryptoUtils.encryptPassword("passwordToEncrypt");
+
+        String decrypted = CryptoUtils.decryptPassword(encrypted);
+
+        assertThat(decrypted).isEqualTo("passwordToEncrypt");
+    }
+
+}

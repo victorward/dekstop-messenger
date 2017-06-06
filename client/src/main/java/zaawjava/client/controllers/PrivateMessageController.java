33d20e57f0fa154c6394c215ac0b6535fc89f8f6
sikora195703 <195703@edu.p.lodz.pl>
2017-06-07 00:04:53 +0200

    fix buga z odtwarzaniem dzwieku z pliku jar

--- client/src/main/java/zaawjava/client/controllers/PrivateMessageController.java
+++ client/src/main/java/zaawjava/client/controllers/PrivateMessageController.java
@@ -1,8 +1,5 @@
 package zaawjava.client.controllers;
 
-import zaawjava.commons.DTO.ChatMessageDTO;
-import zaawjava.commons.DTO.UserDTO;
-
 import com.jfoenix.controls.JFXListView;
 import com.jfoenix.controls.JFXTextArea;
 import io.netty.channel.Channel;
@@ -22,17 +19,21 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
-
 import org.controlsfx.control.Notifications;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import zaawjava.commons.utils.MessageHandler;
 import zaawjava.client.ScreensManager;
 import zaawjava.client.services.SocketService;
 import zaawjava.client.services.UserService;
 import zaawjava.client.utils.ChatMessageCellFactory;
+import zaawjava.commons.DTO.ChatMessageDTO;
+import zaawjava.commons.DTO.UserDTO;
+import zaawjava.commons.utils.MessageHandler;
 
 import java.io.File;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Date;
 import java.util.List;
@@ -44,121 +45,128 @@ import java.util.concurrent.CompletableFuture;
  */
 @Component
 public class PrivateMessageController implements Initializable {
-	private ScreensManager screensManager;
-	private UserService userService;
-	private final SocketService socketService;
+    private static final Logger log = LoggerFactory.getLogger(PrivateMessageController.class);
 
-	private UserDTO userDTO;
+    private ScreensManager screensManager;
+    private UserService userService;
+    private final SocketService socketService;
 
-	@Autowired
-	public void setUserService(UserService userService) {
-		this.userService = userService;
-	}
+    private UserDTO userDTO;
 
-	@Autowired
-	public void setScreensManager(ScreensManager screensManager) {
-		this.screensManager = screensManager;
-	}
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 
-	@Autowired
-	PrivateMessageController(SocketService socketService) {
-		this.socketService = socketService;
-	}
+    @Autowired
+    public void setScreensManager(ScreensManager screensManager) {
+        this.screensManager = screensManager;
+    }
 
-	@FXML
-	private JFXListView<ChatMessageDTO> messagesListView;
+    @Autowired
+    PrivateMessageController(SocketService socketService) {
+        this.socketService = socketService;
+    }
 
-	@FXML
-	private Label userName;
+    @FXML
+    private JFXListView<ChatMessageDTO> messagesListView;
 
-	@FXML
-	private JFXTextArea sendMessageArea;
+    @FXML
+    private Label userName;
 
-	@FXML
-	private ImageView userAvatar;
+    @FXML
+    private JFXTextArea sendMessageArea;
 
-	private ObservableList<ChatMessageDTO> chatMessageDTOS = FXCollections.observableArrayList();
-	private CompletableFuture<Image> prevImageFuture;
+    @FXML
+    private ImageView userAvatar;
 
-	@FXML
-	void sendMessageBtn(ActionEvent event) {
-		sendChatMessage();
-	}
+    private ObservableList<ChatMessageDTO> chatMessageDTOS = FXCollections.observableArrayList();
+    private CompletableFuture<Image> prevImageFuture;
 
-	@Override
-	public void initialize(URL url, ResourceBundle rb) {
-		chatMessageDTOS.clear();
-		messagesListView.setItems(chatMessageDTOS);
-		messagesListView.setCellFactory(new ChatMessageCellFactory());
-		sendMessageArea.setOnKeyPressed(new EventHandler<KeyEvent>() {
-			@Override
-			public void handle(KeyEvent event) {
-				if (event.getCode() == KeyCode.ENTER) {
-					sendChatMessage();
-				}
-			}
-		});
-	}
+    @FXML
+    void sendMessageBtn(ActionEvent event) {
+        sendChatMessage();
+    }
 
-	public void setUserDTO(UserDTO userDTO) {
-		this.userDTO = userDTO;
-		socketService.emit("getConversation", userDTO).whenComplete((msg, ex) -> {
-			Platform.runLater(() -> chatMessageDTOS.addAll((List<ChatMessageDTO>) msg));
-			socketService.on("privateMessage", new MessageHandler() {
-				@Override
-				public void handle(Object msg, Channel channel, ChannelFuture future) {
-					Platform.runLater(() -> {
-						ClassLoader classLoader = getClass().getClassLoader();
-						File file = new File(classLoader.getResource("sounds/tuturu.mp3").getFile());
-						ChatMessageDTO newChatMessage = (ChatMessageDTO) msg;
-						if (newChatMessage.getSender().getId() != userDTO.getId()) {
-							if (newChatMessage.getRecipient().getId() == userService.getUser().getId()) {
-								Media hit = new Media(file.toURI().toString());
-								MediaPlayer mediaPlayer = new MediaPlayer(hit);
-								mediaPlayer.play();
-								Notifications.create().position(Pos.BOTTOM_RIGHT)
-										.text(newChatMessage.getSender().getFirstName() + " "
-												+ newChatMessage.getSender().getLastName() + ": \n"
-												+ newChatMessage.getContent())
-										.show();
-							}
-						}
-						if (newChatMessage.getSender().getId() == userDTO.getId()
-								|| newChatMessage.getSender().getId() == userService.getUser().getId()) {
-							chatMessageDTOS.add(newChatMessage);
-						}
-					});
-				}
-			});
-		});
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        chatMessageDTOS.clear();
+        messagesListView.setItems(chatMessageDTOS);
+        messagesListView.setCellFactory(new ChatMessageCellFactory());
+        sendMessageArea.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode() == KeyCode.ENTER) {
+                    sendChatMessage();
+                }
+            }
+        });
+    }
 
-		if (userDTO != null) {
-			userName.setText("");
-			userName.setText(userDTO.getFirstName() + " " + userDTO.getLastName());
-			Platform.runLater(() -> setProfileAvatar(userDTO));
-		}
+    public void setUserDTO(UserDTO userDTO) {
+        this.userDTO = userDTO;
+        socketService.emit("getConversation", userDTO).whenComplete((msg, ex) -> {
+            Platform.runLater(() -> chatMessageDTOS.addAll((List<ChatMessageDTO>) msg));
+            socketService.on("privateMessage", new MessageHandler() {
+                @Override
+                public void handle(Object msg, Channel channel, ChannelFuture future) {
+                    Platform.runLater(() -> {
+                        ClassLoader classLoader = getClass().getClassLoader();
+                        ChatMessageDTO newChatMessage = (ChatMessageDTO) msg;
+                        if (newChatMessage.getSender().getId() != userDTO.getId()) {
+                            if (newChatMessage.getRecipient().getId() == userService.getUser().getId()) {
+                                Media hit = null;
+                                try {
+                                    hit = new Media(classLoader.getResource("sounds/tuturu.mp3").toURI().toString());
+                                    MediaPlayer mediaPlayer = new MediaPlayer(hit);
+                                    mediaPlayer.play();
+                                } catch (URISyntaxException e) {
+                                    log.warn("cannot load resource:", e);
+                                }
 
-	}
+                                Notifications.create().position(Pos.BOTTOM_RIGHT)
+                                        .text(newChatMessage.getSender().getFirstName() + " "
+                                                + newChatMessage.getSender().getLastName() + ": \n"
+                                                + newChatMessage.getContent())
+                                        .show();
+                            }
+                        }
+                        if (newChatMessage.getSender().getId() == userDTO.getId()
+                                || newChatMessage.getSender().getId() == userService.getUser().getId()) {
+                            chatMessageDTOS.add(newChatMessage);
+                        }
+                    });
+                }
+            });
+        });
 
-	void setProfileAvatar(UserDTO userDTO) {
-		String imageSource = userDTO.getPhoto();
-		if (imageSource != null && !imageSource.equals("")) {
-			if (prevImageFuture != null && !prevImageFuture.isDone()) {
-				prevImageFuture.cancel(false);
-			}
-			prevImageFuture = CompletableFuture.supplyAsync(() -> new Image(imageSource)).whenComplete((img, ex) -> {
-				Platform.runLater(() -> userAvatar.setImage(img));
-			});
-		}
-	}
+        if (userDTO != null) {
+            userName.setText("");
+            userName.setText(userDTO.getFirstName() + " " + userDTO.getLastName());
+            Platform.runLater(() -> setProfileAvatar(userDTO));
+        }
 
-	private void sendChatMessage() {
-		if (sendMessageArea.getText().trim().length() == 0)
-			return;
-		ChatMessageDTO message = new ChatMessageDTO(userService.getUser(), sendMessageArea.getText().trim());
-		message.setRecipient(userDTO);
-		message.setDate(new Date());
-		sendMessageArea.setText("");
-		socketService.emit("newPrivateMessage", message);
-	}
+    }
+
+    void setProfileAvatar(UserDTO userDTO) {
+        String imageSource = userDTO.getPhoto();
+        if (imageSource != null && !imageSource.equals("")) {
+            if (prevImageFuture != null && !prevImageFuture.isDone()) {
+                prevImageFuture.cancel(false);
+            }
+            prevImageFuture = CompletableFuture.supplyAsync(() -> new Image(imageSource)).whenComplete((img, ex) -> {
+                Platform.runLater(() -> userAvatar.setImage(img));
+            });
+        }
+    }
+
+    private void sendChatMessage() {
+        if (sendMessageArea.getText().trim().length() == 0)
+            return;
+        ChatMessageDTO message = new ChatMessageDTO(userService.getUser(), sendMessageArea.getText().trim());
+        message.setRecipient(userDTO);
+        message.setDate(new Date());
+        sendMessageArea.setText("");
+        socketService.emit("newPrivateMessage", message);
+    }
 }
